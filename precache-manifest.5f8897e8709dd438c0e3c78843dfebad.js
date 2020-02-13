f02db0d61daa79fcd28fc10104e17e1c432f913c
Yosua Tarigan <yosuatarigan7@gmail.com>
2020-02-13 18:53:57 +0700

    Updates

---
+++ precache-manifest.5f8897e8709dd438c0e3c78843dfebad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e4032cefac665710cd8536aa662ed6",
+    "url": "/robofriends-react-redux/index.html"
+  },
+  {
+    "revision": "dc39f384bfad1c1c07e9",
+    "url": "/robofriends-react-redux/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "be8e7733d0e525274366",
+    "url": "/robofriends-react-redux/static/css/main.bb4f08b1.chunk.css"
+  },
+  {
+    "revision": "dc39f384bfad1c1c07e9",
+    "url": "/robofriends-react-redux/static/js/2.4952670c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/robofriends-react-redux/static/js/2.4952670c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be8e7733d0e525274366",
+    "url": "/robofriends-react-redux/static/js/main.2df07552.chunk.js"
+  },
+  {
+    "revision": "6177f41abe80630745b2",
+    "url": "/robofriends-react-redux/static/js/runtime-main.dafef853.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
